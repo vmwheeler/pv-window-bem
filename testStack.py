afb5ecb2c33152b8a56be0e94c47091d15c328b6
Vincent Wheeler <vincent.m.wheeler@gmail.com>
2020-06-01 00:07:09 -0500

    developed stack class... calculates solar weighted absorption using AM1.5 spectrum.

--- testStack.py
+++ testStack.py
@@ -12,23 +12,41 @@ degree = np.pi/180
 #inc_angle = 10.*degree
 inc_angle = 0.*degree
         
-num_lams = 500
-lams = np.linspace(0.3,2.5,num=num_lams)
+#num_lams = 500
+#lams = np.linspace(0.3,2.5,num=num_lams)
+lamrange = [0.3,2.5]
 
-Glass = Layer(4000,'Rubin-clear_dumb','i',onecol=True)
-TiO2 = Layer(0.05,'Siefke','c',onecol=True)
-AZO = Layer(0.2,'Treharne','c',onecol=True)
-MAPI = Layer(0.5,'Phillips','c')
-ITO = Layer(0.2,'Moerland','c',onecol=True)
-ZnO = Layer(0.05,'Stelling','c',onecol=True)
-PVP = Layer(1500,'Konig','i',onecol=True)
+Glass = Layer(4000,'nkLowFeGlass','i')
+TiO2 = Layer(0.05,'nkTiO2','c')
+FTO = Layer(0.3,'nkFTO','c')
+MAPI = Layer(0.5,'nkMAPI','c')
+ITO = Layer(0.4,'nkITO','c')
+SnO2 = Layer(0.5,'nkSnO2','c')
+NiO = Layer(0.05,'nkNiO','c')
+Ag = Layer(0.01,'nkAg','c')
+TiO2lowE = Layer(0.02,'nkTiO2','c')
+Bleach = Layer(0.5,'nkTiO2','c')
+EVA = Layer(1500,'nkEVA','i')
 
 
-layers = [Glass,ITO,TiO2,MAPI,ZnO,AZO,PVP,Glass]
+#MAPI.plotnk(lams)
+
+
+layers = [Glass,FTO,TiO2,Bleach,NiO,ITO,EVA,Glass,TiO2lowE,Ag,TiO2lowE]
+#layers = [MAPI]
 
 stack = Stack(layers)
 
-Glass.plotnk(lams)
+A_sol = stack.get_solar_weighted_absorption(lamrange,inc_angle)
+
+print("A_sol = " + str(A_sol))
+
+'''
+plt.figure()
+plt.plot(lams,stack.Is(lams))
+plt.show()
+'''
+
 
 
 """
